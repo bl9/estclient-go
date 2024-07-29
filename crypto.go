ecd3cb387dc14615809ed3c42011302255bf3569
lrwx00t <lrwx00t@gmail.com>
2024-07-28 20:39:27 -0400

    added csrattrs

--- crypto.go
+++ crypto.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/asn1"
+	"encoding/base64"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os/exec"
+	"unicode"
 )
 
 var oidEmailAddress = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 9, 1}
@@ -52,12 +58,19 @@ func GenCSR() ([]byte, []byte) {
 		Type:  "CERTIFICATE REQUEST",
 		Bytes: csrBytes,
 	}
+	csrBytes = pem.EncodeToMemory(&block)
 	return privKey, csrBytes
 }
 
-func GetWithCA(ca_file, url string) []byte {
+// read PKCS7 response
+// cat base_pkcs| base64 -d | openssl pkcs7 -inform DER -outform PEM -print_certs
+func GetWithCA(ca_file, url string, headers map[string]string, data []byte, method string, username string, password string) []byte {
 	// Load the CA certificate
 	// wget http://testrfc7030.com/dstcax3.pem
+	m := "GET"
+	if method != "" {
+		m = method
+	}
 	caCert, err := ioutil.ReadFile(ca_file)
 	if err != nil {
 		fmt.Println("Error reading CA certificate:", err)
@@ -73,17 +86,44 @@ func GetWithCA(ca_file, url string) []byte {
 		},
 	}
 
-	// Create a custom HTTP client with the transport
 	client := &http.Client{
 		Transport: transport,
 	}
-	// url := est_url + "/.well-known/est/cacerts"
-	resp, err := client.Get(url)
+	// Create a new request with custom headers
+	req, err := http.NewRequest(m, url, bytes.NewReader(data))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error creating request:", err)
+	}
+
+	// Add custom headers
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	if username != "" && password != "" {
+		req.SetBasicAuth(username, password)
+	}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
 	}
 	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
+
+	// Read and print the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+	// fmt.Println("---------------")
+	// fmt.Println(string(body))
+	// fmt.Println("---------------")
+	// fmt.Println(resp.Status)
+	// output, _ := pkcs7ToPem(body)
+	// fmt.Println(output)
+	// fmt.Println("%v", resp.Header)
+
 	return body
 }
 
@@ -91,13 +131,75 @@ func GetWithCA(ca_file, url string) []byte {
 // openssl base64 -d -in cacerts.p7m | openssl pkcs7 -inform DER -outform PEM -print_certs
 func GetCaCert(est_url string) []byte {
 	url := est_url + "/.well-known/est/cacerts"
-	return GetWithCA("dstcax3.pem", url)
+	return GetWithCA("dstcax3.pem", url, nil, nil, "", "", "")
 }
 
-// func SimpleEnroll(csr []bytes) {
-// 	url := c.URLPrefix + "/.well-known/est/simpleenroll"
-// 	headers := map[string]string{
-// 		"Content-Type": "application/pkcs10",
-// 	}
+func SimpleEnroll(est_url string, csr []byte) []byte {
+	url := est_url + "/.well-known/est/simpleenroll"
+	headers := map[string]string{
+		"Content-Type": "application/pkcs10",
+	}
+	return GetWithCA("dstcax3.pem", url, headers, csr, "POST", "estuser", "estpwd")
+}
 
-// }
+func SimpleReEnroll(est_url string, csr []byte) []byte {
+	url := est_url + "/.well-known/est/simplereenroll"
+	headers := map[string]string{
+		"Content-Type": "application/pkcs10",
+	}
+	return GetWithCA("dstcax3.pem", url, headers, csr, "POST", "estuser", "estpwd")
+}
+
+func pkcs7ToPem(pkcs7 []byte) (string, error) {
+	var inform string
+	var out, err bytes.Buffer
+
+	for _, filetype := range []string{"PEM", "DER"} {
+		cmd := exec.Command("openssl", "pkcs7", "-inform", filetype, "-outform", "PEM", "-print_certs")
+		cmd.Stdin = bytes.NewReader(pkcs7)
+
+		cmd.Stdout = &out
+		cmd.Stderr = &err
+
+		run_err := cmd.Run()
+		if run_err == nil {
+			inform = filetype
+			break
+		}
+	}
+
+	if inform == "" {
+		return "", errors.New("invalid PKCS7 data type")
+	}
+
+	return fmt.Sprintf(out.String(), err.String()), nil
+}
+
+// curl https://testrfc7030.com:8443/.well-known/est/csrattrs -s --cacert ./dstcax3.pem | openssl base64 -d -A | openssl asn1parse -inform DER
+func CSRAttr(est_url string, csr []byte) asn1.RawValue {
+	url := est_url + "/.well-known/est/csrattrs"
+	bin_data := GetWithCA("dstcax3.pem", url, nil, csr, "", "estuser", "estpwd")
+	bin_data_str := removeNonPrintable(string(bin_data))
+	data, err := base64.StdEncoding.DecodeString(string(bin_data_str))
+	if err != nil {
+		log.Fatalf("Error decoding base64 string: %v", err)
+	}
+
+	// Parse ASN.1 DER encoded data
+	var parsedData asn1.RawValue
+	_, err = asn1.Unmarshal(data, &parsedData)
+	if err != nil {
+		log.Fatalf("Error parsing ASN.1 DER data: %v", err)
+	}
+	return parsedData
+}
+
+func removeNonPrintable(input string) string {
+	var result []rune
+	for _, r := range input {
+		if unicode.IsPrint(r) {
+			result = append(result, r)
+		}
+	}
+	return string(result)
+}
--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 )
 
 func main() {
-	// p, c := GenCSR()
-	// fmt.Println(string(c))
-	// fmt.Println(string(p))
 	est_server_url := "https://testrfc7030.com:8443"
 	fmt.Println(string(GetCaCert(est_server_url)))
+	_, c := GenCSR()
+	fmt.Println(string(SimpleEnroll(est_server_url, c)))
+	fmt.Println(string(SimpleReEnroll(est_server_url, c)))
+	parsedData := CSRAttr(est_server_url, c)
+	fmt.Println("Parsed ASN.1 Data:")
+	fmt.Printf("Tag: %d\n", parsedData.Tag)
+	fmt.Printf("Class: %d\n", parsedData.Class)
+	fmt.Printf("Is Compound: %v\n", parsedData.IsCompound)
+	fmt.Printf("Bytes: %x\n", parsedData.Bytes)
+
 }
